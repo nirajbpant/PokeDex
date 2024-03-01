4d14cf5572ec9593e704882fe1654416df99ec6f
nirajbpant <nirajbpant@gmail.com>
2024-03-01 10:55:24 -0500

    Add color, shapes and fonts.

--- app/src/main/java/com/example/pokedex/presentation/pokemon_list/PokemonList.kt
+++ app/src/main/java/com/example/pokedex/presentation/pokemon_list/PokemonList.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pokedex.presentation.pokemon_list.ui.theme.PokeDexTheme
+import com.example.pokedex.presentation.theme.PokeDexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
